91d299e14c6f011f428685af4be7c501d8f2c03e
hw0k <hw0knam@gmail.com>
2020-04-01 20:07:25 +0900

    코드 구조 일부 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import { AppProps } from 'next/app';
 
-import '../styles/index.css';
+import '../assets/css/index.css';
 
 // eslint-disable-next-line react/jsx-props-no-spreading
 const App: React.FC<AppProps> = ({ Component, pageProps }) => <Component {...pageProps} />;
